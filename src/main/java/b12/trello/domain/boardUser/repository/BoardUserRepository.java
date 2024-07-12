f7c04344f678ec6ca81291345be44af2694a8753
shinmink <74857932+shinmink@users.noreply.github.com>
2024-07-12 17:35:27 +0900

    FIX: #7 BoardUserRepository default throw 추가 FIX: #7 BoardUserException 추가

--- src/main/java/b12/trello/domain/boardUser/repository/BoardUserRepository.java
+++ src/main/java/b12/trello/domain/boardUser/repository/BoardUserRepository.java
@@ -3,6 +3,8 @@ package b12.trello.domain.boardUser.repository;
 import b12.trello.domain.board.entity.Board;
 import b12.trello.domain.boardUser.entity.BoardUser;
 import b12.trello.domain.user.entity.User;
+import b12.trello.global.exception.customException.BoardUserException;
+import b12.trello.global.exception.errorCode.BoardUserErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -16,4 +18,10 @@ public interface BoardUserRepository extends JpaRepository<BoardUser, Long> {
     List<BoardUser> findByUser(User user);
 
     List<BoardUser> findByUserAndBoardUserRole(User user, BoardUser.BoardUserRole boardUserRole);
+
+    Optional<BoardUser> findByBoardIdAndUserId(Long boardId, Long userId);
+
+    default BoardUser findByBoardIdAndUserIdOrElseThrow(Long boardId, Long userId) {
+        return findByBoardIdAndUserId(boardId, userId).orElseThrow(() -> new BoardUserException(BoardUserErrorCode.BOARD_USER_NOT_FOUND));
+    }
 }

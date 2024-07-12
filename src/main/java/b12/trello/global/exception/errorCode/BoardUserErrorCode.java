73d681f525959ee9df84e89b8fb2c0b655edb4f7
shinmink <74857932+shinmink@users.noreply.github.com>
2024-07-12 19:21:31 +0900

    FIX: #7 Default refactoring and builder fixes 수정, 추가

--- src/main/java/b12/trello/global/exception/errorCode/BoardUserErrorCode.java
+++ src/main/java/b12/trello/global/exception/errorCode/BoardUserErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum BoardUserErrorCode implements ErrorCode{
 
-    BOARD_USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 사용자입니다.");
+    BOARD_USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "보드 사용자 정보를 찾을 수 없습니다.");
 
     private final int httpStatusCode;
     private final String errorDescription;

7ebe6bc2ea2e6466f58f1d264e94cb631931108c
Hoonable <161635302+Hoonable@users.noreply.github.com>
2024-07-12 14:30:02 +0900

    REFACTOR: #4 columnService 리팩토링 중복 파트 수정, 주석 추가

--- src/main/java/b12/trello/domain/column/service/ColumnService.java
+++ src/main/java/b12/trello/domain/column/service/ColumnService.java
@@ -28,59 +28,54 @@ public class ColumnService {
     private final ColumnRepository columnRepository;
     private final BoardRepository boardRepository;
 
-    public void createColumn(ColumnCreateRequestDto requestDto) {
-        Board board = boardRepository.findById(requestDto.getBoardId())
-            .orElseThrow(() -> new BoardNotFoundException(
-                ColumnErrorCode.BOARD_NOT_FOUND));
-        if (board.getBoardStatus() == BoardStatus.DELETED) {
-            throw new BoardNotFoundException(ColumnErrorCode.DELETED_BOARD);
-        }
-        if (columnRepository.existsByColumnNameAndBoardId(requestDto.getColumnName(),
-            requestDto.getBoardId())) {
-            throw new ColumnDuplicatedException(ColumnErrorCode.COLUMN_ALREADY_REGISTERED_ERROR);
-        }
 
-        Long columnOrder = columnRepository.countByBoardId(requestDto.getBoardId());
+    //컬럼생성
+    public void createColumn(ColumnCreateRequestDto requestDto) {
+
+        Board board = checkBoard(requestDto.getBoardId());
+
+        checkColumnName(requestDto.getColumnName(), requestDto.getBoardId());
+
+        Long columnOrder = columnRepository.countByBoardId(requestDto.getBoardId()); //컬럼 마지막 순서 계산
+
         Columns columns = Columns.builder().board(board).columnName(requestDto.getColumnName())
-            .order(columnOrder).build();
+            .order(columnOrder).build(); //컬럼 생성
+
         columnRepository.save(columns);
     }
 
-
+    //컬럼 조회
     public List<ColumnFindResponseDto> findColumns(ColumnFindRequestDto requestDto) {
 
-        Board board = boardRepository.findById(requestDto.getBoardId())
-            .orElseThrow(() -> new BoardNotFoundException(
-                ColumnErrorCode.BOARD_NOT_FOUND));
-        if (board.getBoardStatus() == BoardStatus.DELETED) {
-            throw new BoardNotFoundException(ColumnErrorCode.DELETED_BOARD);
-        }
+        checkBoard(requestDto.getBoardId());
 
         List<ColumnFindResponseDto> columns = columnRepository.findAllByBoardIdOrderByColumnOrderAsc(
-            requestDto.getBoardId()).stream().map(
-            ColumnFindResponseDto::new).toList();
+            requestDto.getBoardId()).stream().map(ColumnFindResponseDto::new).toList(); //순서 오름차순으로 컬럼조회
 
         return columns;
-
     }
 
+    //컬럼 수정
     public void modifyColumn(Long columnId, ColumnModifyRequestDto requestDto) {
-        Columns columns = columnRepository.findById(columnId)
-            .orElseThrow(() -> new ColumnNotFoundException(ColumnErrorCode.COLUMN_NOT_FOUND));
-        if (columnRepository.existsByColumnNameAndBoardId(requestDto.getColumnName(),
-            columns.getBoard().getId())) {
-            throw new ColumnDuplicatedException(ColumnErrorCode.COLUMN_ALREADY_REGISTERED_ERROR);
-        }
+
+        Columns columns = checkColumn(columnId);
+
+        checkColumnName(requestDto.getColumnName(), columns.getBoard().getId());
+
         columns.setColumnName(requestDto.getColumnName());
+
         columnRepository.save(columns);
     }
 
+    //컬럼 삭제
     @Transactional
     public void deleteColumn(Long columnId) {
 
-        Columns columns = columnRepository.findById(columnId)
-            .orElseThrow(() -> new ColumnNotFoundException(ColumnErrorCode.COLUMN_NOT_FOUND));
-        columnRepository.deleteById(columnId);
+        Columns columns = checkColumn(columnId);
+
+        columnRepository.deleteById(columnId); //컬럼 삭제
+
+        //컬럼 순서 조정
         List<Columns> columnsList = columnRepository.findAllByBoardIdAndColumnOrderGreaterThan(
             columns.getBoard().getId(), columns.getColumnOrder());
         for (Columns column : columnsList) {
@@ -94,16 +89,17 @@ public class ColumnService {
 
         Long newOrder = requestDto.getOrderId();
 
-        Columns columns = columnRepository.findById(columnId)
-            .orElseThrow(() -> new ColumnNotFoundException(ColumnErrorCode.COLUMN_NOT_FOUND));
+        Columns columns = checkColumn(columnId);
 
         Long boardId = columns.getBoard().getId();
-        Long maxOrder = columnRepository.countByBoardId(boardId) - 1;
 
+        // valid 순서인지 확인
+        Long maxOrder = columnRepository.countByBoardId(boardId) - 1;
         if (newOrder < 0 || newOrder > maxOrder) {
             throw new InvalidOrderException(ColumnErrorCode.INVALID_ORDER);
         }
 
+        // 순서 변경 로직
         if (Objects.equals(columns.getColumnOrder(), newOrder)) {
             throw new InvalidOrderException(ColumnErrorCode.INVALID_ORDER);
         } else if (columns.getColumnOrder() > newOrder) {
@@ -127,6 +123,29 @@ public class ColumnService {
                 column.setColumnOrder(column.getColumnOrder() - 1);
             }
         }
+    }
 
+    //보드 확인
+    private Board checkBoard(Long boardId) {
+        Board board = boardRepository.findById(boardId)
+            .orElseThrow(() -> new BoardNotFoundException(
+                ColumnErrorCode.BOARD_NOT_FOUND));
+        if (board.getBoardStatus() == BoardStatus.DELETED) {
+            throw new BoardNotFoundException(ColumnErrorCode.DELETED_BOARD);
+        }
+        return board;
+    }
+
+    //컬럼 이름 중복 확인
+    private void checkColumnName(String columnName, Long boardId) {
+        if (columnRepository.existsByColumnNameAndBoardId(columnName, boardId)) {
+            throw new ColumnDuplicatedException(ColumnErrorCode.COLUMN_ALREADY_REGISTERED_ERROR);
+        }
+    }
+
+    //컬럼 존재 확인
+    private Columns checkColumn(Long columnId) {
+        return columnRepository.findById(columnId)
+            .orElseThrow(() -> new ColumnNotFoundException(ColumnErrorCode.COLUMN_NOT_FOUND));
     }
 }

3e6f938f3dd21074ba9db665ee6132889d470c72
shinmink <74857932+shinmink@users.noreply.github.com>
2024-07-13 23:18:17 +0900

    FEAT: #17 댓글 등록 기능 추가 FEAT: #17 특정 카드 안 댓글들 전체 조회 FEAT: #17 댓글 수정 기능 추가 FEAT: #17 댓글 삭제 기능 추가

--- src/main/java/b12/trello/domain/comment/service/CommentService.java
+++ src/main/java/b12/trello/domain/comment/service/CommentService.java
@@ -1,7 +1,77 @@
 package b12.trello.domain.comment.service;
 
+import b12.trello.domain.card.entity.Card;
+import b12.trello.domain.card.repository.CardRepository;
+import b12.trello.domain.comment.dto.CommentRequestDto;
+import b12.trello.domain.comment.dto.CommentResponseDto;
+import b12.trello.domain.comment.entity.Comment;
+import b12.trello.domain.comment.repository.CommentRepository;
+import b12.trello.domain.user.entity.User;
+import b12.trello.global.exception.customException.CardException;
+import b12.trello.global.exception.customException.CommentException;
+import b12.trello.global.exception.errorCode.CardErrorCode;
+import b12.trello.global.exception.errorCode.CommentErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CardRepository cardRepository;
+
+    @Transactional
+    public CommentResponseDto createComment(CommentRequestDto commentRequestDto, User user) {
+        Card card = cardRepository.findById(commentRequestDto.getCardId())
+                .orElseThrow(() -> new CardException(CardErrorCode.CARD_NOT_FOUND));
+
+        Comment comment = Comment.builder()
+                .card(card)
+                .user(user)
+                .Contents(commentRequestDto.getContent())
+                .build();
+
+        commentRepository.save(comment);
+
+        return new CommentResponseDto(comment);
+    }
+
+    // 댓글 전체 조회
+    @Transactional(readOnly = true)
+    public List<CommentResponseDto> findAllByCardId(Long cardId) {
+        List<Comment> comments = commentRepository.findAllByCardIdOrderByCreatedAtDesc(cardId);
+        return comments.stream()
+                .map(CommentResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentResponseDto modifyComment(Long commentId, CommentRequestDto commentRequestDto, User user) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CardException(CardErrorCode.CARD_NOT_FOUND));
+
+        if (!comment.getUser().getId().equals(user.getId())) {
+            throw new CommentException(CommentErrorCode.ACCESS_DENIED);
+        }
+
+        comment.update(commentRequestDto.getContent());
+        return new CommentResponseDto(comment);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId, User user) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CardException(CardErrorCode.CARD_NOT_FOUND));
+
+        if (!comment.getUser().getId().equals(user.getId())) {
+            throw new CommentException(CommentErrorCode.ACCESS_DENIED);
+        }
+
+        commentRepository.delete(comment);
+    }
 }

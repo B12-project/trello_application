9b52944e5a113a40484d54a9096fb19ce764716b
Hoonable <161635302+Hoonable@users.noreply.github.com>
2024-07-13 17:28:50 +0900

    REFACTOR: #4 ColumnException 수정
    ColumnException으로 통일

--- src/main/java/b12/trello/global/exception/customException/column/ColumnNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package b12.trello.global.exception.customException.column;
-
-import b12.trello.global.exception.customException.GlobalCustomException;
-import b12.trello.global.exception.errorCode.ErrorCode;
-
-public class ColumnNotFoundException extends GlobalCustomException{
-    public ColumnNotFoundException(ErrorCode errorCode) {super(errorCode);}
-}
--- src/main/java/b12/trello/global/exception/customException/column/InvalidUserException.java
+++
@@ -1,8 +0,0 @@
-package b12.trello.global.exception.customException.column;
-
-import b12.trello.global.exception.customException.GlobalCustomException;
-import b12.trello.global.exception.errorCode.ErrorCode;
-
-public class InvalidUserException extends GlobalCustomException {
-    public  InvalidUserException(ErrorCode errorCode) {super(errorCode);}
-}
